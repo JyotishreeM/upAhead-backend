3b83a005d499f350709b1d6590a7e74256193284
Jyotishree.Majumdar <siladitya.datta@custobridge.com>
2026-09-16 11:04:41 +0530

    Allow production frontend in CORS

--- src/main/java/com/upahead/backend/security/SecurityConfig.java
+++ src/main/java/com/upahead/backend/security/SecurityConfig.java
@@ -66,7 +66,10 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:4200"));
+                List.of(
+                    "http://localhost:4200",
+                    "https://upahead-frontend-production.up.railway.app"
+                    ));
 
         configuration.setAllowedMethods(
                 List.of(
